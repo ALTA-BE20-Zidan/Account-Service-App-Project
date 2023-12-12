4afeb9a0b6dd17e4d30ad5e92d9f2303587ef683
zidan970 <amdzidan70@gmail.com>
2023-12-12 20:53:09 +0700

    ubah nama function

--- controllers/AccountsContollers.go
+++ controllers/AccountsContollers.go
@@ -7,7 +7,7 @@ import (
 	"zidan/AccountServiceAppProject/entities"
 )
 
-func AddAccountControllers(db *sql.DB) {
+func AddAccountController(db *sql.DB) {
 	var newAccount entities.Accounts
 	newAccount.User_balance = 0
 
@@ -47,4 +47,25 @@ func AddAccountControllers(db *sql.DB) {
 		fmt.Println("Succesfully insert data. Row affected ID:", hasilRows)
 	}
 	fmt.Println()
+}
+
+func ReadAccountControllerr(db *sql.DB) []entities.Accounts {
+	// *** read / select data all_accounts *** //
+	var All_accounts []entities.Accounts
+
+	rows, errSelect := db.Query("select * from accounts")
+	if errSelect != nil {
+		log.Fatal("cannot run select query: ", errSelect)
+	}
+
+	for rows.Next() {
+		var Row_accounts entities.Accounts
+		errScan := rows.Scan(&Row_accounts.User_id, &Row_accounts.Username, &Row_accounts.User_nama, &Row_accounts.User_phone, &Row_accounts.User_email, &Row_accounts.User_address, &Row_accounts.User_balance, &Row_accounts.User_pswd)
+		if errScan != nil {
+			log.Fatal("cannot run scan query: ", errScan.Error())
+		}
+		All_accounts = append(All_accounts, Row_accounts)
+	}
+
+	return All_accounts
 }
