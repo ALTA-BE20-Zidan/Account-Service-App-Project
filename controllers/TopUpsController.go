634a370b9ce9516bc3efd0a8689b898a65878eb7
zidan970 <amdzidan70@gmail.com>
2023-12-13 15:57:01 +0700

    add function for checking amount format

--- controllers/TopUpsController.go
+++ controllers/TopUpsController.go
@@ -1,1 +1,18 @@
 package controllers
+
+import (
+	"database/sql"
+	"strings"
+)
+
+func FormatCheckController(db *sql.DB, amount string) bool {
+	var valid string = "0123456789"
+
+	for _, karakter := range amount {
+		if !strings.Contains(valid, string(karakter)) {
+			return false
+		}
+	}
+
+	return true
+}
