32dec78074d3efdb8e8f4b5b6119951c70ee9e2d
zidan970 <amdzidan70@gmail.com>
2023-12-14 10:03:53 +0700

    add struct for transfers

--- entities/transfers.go
+++ entities/transfers.go
@@ -1,1 +1,11 @@
 package entities
+
+import "time"
+
+type Transfers struct {
+	Transfer_id     uint
+	User_id_sdr     uint
+	User_id_rcv     uint
+	Transfer_amount uint
+	Transfer_time   time.Time
+}
